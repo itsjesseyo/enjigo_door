8d48e0ddaafed3c2f28bd66c5dd0ae985a371e4a
Jesse Gomez <jesse@luxnovalabs.com>
2014-02-23 19:50:05 -0700

    i2c blinkm a chance to breath

--- door_interface/helpers/led.py
+++ door_interface/helpers/led.py
@@ -1,5 +1,5 @@
 from pyblinkm import BlinkM, Scripts
-import time
+import datetime, time
 
 class Led():
 
@@ -7,21 +7,30 @@ class Led():
 		#so we can have many different leds
 		self.name = name
 		self.led = BlinkM()
+		self.delay(2)
 		self.led.reset()
+		self.delay(1)
 		self.led.set_fade_speed(32)
-		
+		self.delay(1)
+	
+	def delay(self, time):
+		time.sleep(time)
 
 	def show_status_light(self):
 		self.led.fade_to_hex("0000ff")
+		self.delay(1)
 
 	def show_thinking_light(self):
 		#self.led.play_script(Scripts.WHITE_FLASH)
 		self.led.fade_to_hex("ffffff")
+		self.delay(1)
 
 	def show_positive_light(self):
 		#self.led.stop_script()
 		self.led.fade_to_hex("00ff00")
+		self.delay(1)
 
 	def show_negative_light(self):
 		#self.led.stop_script()
 		self.led.fade_to_hex("ff0000")
+		self.delay(1)
